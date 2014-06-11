26df7729690ddd5bc5e6ef3196ba406ba67633f2
Rizwan Mansuri <mr.mansuri@gmail.com>
2014-06-11 16:35:00 +0100

    Fixed CourseDate preview

---
+++ views/public/CourseDate/view/render.py
@@ -0,0 +1,10 @@
+#Script (Python) "render_view
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=
+##
+return context.render_helper(version=context.REQUEST.model.get_previewable())
