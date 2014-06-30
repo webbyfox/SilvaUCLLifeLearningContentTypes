1461f905a1295fbd634ff77b351c61a139128714
Rizwan Mansuri <mr.mansuri@gmail.com>
2014-06-30 15:45:44 +0100

    Course case study page object added

---
+++ content/CourseSubject.py
@@ -0,0 +1,160 @@
+# Copyright (c) 2014 University College London. All rights reserved.
+
+
+# Python
+from StringIO import StringIO
+from xml.sax import parseString
+from xml.sax.handler import ContentHandler 
+
+# Zope
+from AccessControl import ClassSecurityInfo
+from DateTime import DateTime
+from Globals import InitializeClass
+from OFS.SimpleItem import SimpleItem
+from zope.interface import implements
+
+
+# Silva
+from Products.Silva import SilvaPermissions
+from Products.Silva.Folder import Folder
+from Products.Silva import mangle
+from Products.Silva.VersionedContent import CatalogedVersionedContent
+from Products.Silva.Version import CatalogedVersion
+
+# not sure next three lines uses in code or not...removed it if not
+from Products.Silva.interfaces import IVersionedContent
+from Products.Silva.helpers import add_and_edit
+from Products.Silva.Metadata import export_metadata
+
+
+
+# Others
+from interfaces import ICourseSubject, ICourseSubjectVersion
+from Products.SilvaUCLLifeLearningContentTypes.silvaxmlattribute import SilvaXMLAttribute
+
+
+class MetaDataSaveHandler(ContentHandler):
+    
+    def startDocument(self):
+        self.title = "" 
+        self.inside_title = False
+        self.metadata = {}
+    
+    def startElement(self, name, attributes):
+        if name == 'h2' and not self.title:
+            self.inside_title = True
+        elif name == 'meta':
+             if (attributes.get('scheme') ==
+                    'http://infrae.com/namespaces/metadata/silva-news'):
+                name = attributes.get("name","")
+                content = attributes.get("content", "")
+                self.metadata[name] = self.parse_content(content)
+
+    def endElement(self, name):
+        if name == 'h2':
+            self.inside_title = False
+
+    def characters(self, data):
+        if self.inside_title:
+            self.title += data
+            
+    def parse_content(self, content):
+        return [self.deentitize_and_deescape_pipes(x) for x in content.split('|')]                                    
+
+    def deentitize_and_deescape_pipes(self, data):
+        data = data.replace('&pipe;','|')
+        data = data.replace('&lt;','<')
+        data = data.replace('&gt;','>')
+        data = data.replace('&quot;','"')
+        data = data.replace('&amp;','&')
+        return data
+                                
+class CourseSubject(CatalogedVersionedContent):
+    """
+    Simple Course subject object for Course
+    """
+    implements(ICourseSubject)
+    security = ClassSecurityInfo()
+    meta_type = "Course Subject"
+    __doc__ = "Course Subject Object"
+
+    security.declareProtected(SilvaPermissions.ChangeSilvaContent,'PUT')
+    def PUT(self, REQUEST=None, RESPONSE=None):
+        """PUT support
+        """
+        
+        if REQUEST is None:
+            REQUEST = self.REQUEST
+        if RESPONSE is None:
+            RESPONSE = self.RESPONSE
+        
+        content = REQUEST['BODYFILE'].read()
+        self.save_title_and_metadata(content)
+        self.get_editable().content.saveEditorHTML(content)
+
+    def save_title_and_metadata(self, html):
+        handler = MetaDataSaveHandler()
+        parseString(html, handler)
+        version = self.get_editable()
+        if ICourseSubjectVersion.providedBy(version):
+            version.set_shortdescription(handler.metadata['shortdescription'][0])
+            version.set_image(handler.metadata['image'][0])
+            version.set_casestudy(handler.metadata['casestudy'][0]) 
+        version.set_title(handler.title)
+
+class CourseSubjectVersion(CatalogedVersion):           
+    """Base class for course object versions
+    """        
+    implements(ICourseSubjectVersion)
+    security = ClassSecurityInfo()
+    meta_type = "Course Subject Version"
+
+    def __init__(self,id):
+        super(CourseSubjectVersion, self).__init__(id)
+       
+        self._shortdescription = ""
+        self._image = ""
+        self._casestudy = ""
+        self.content = SilvaXMLAttribute('content') 
+        
+    
+    # Mutator
+    def set_shortdescription(self, shortdescription):
+        self._shortdescription = shortdescription
+
+    # Accessor
+    def get_shortdescription(self):
+        if not hasattr(self, '_shortdescription'):
+            self._shortdescription = ""
+        else:
+            return self._shortdescription
+
+  
+    # Mutator
+    def set_image(self,image):
+        self._image = image
+
+    # Accessor
+    def get_image(self):
+        if not hasattr(self, '_image'):
+            self._image = ""
+        else:
+            return self._image
+     
+
+    # Mutator
+    def set_casestudy(self,casestudy):
+        self._casestudy = casestudy
+
+    # Accessor
+    def get_casestudy(self):
+        if not hasattr(self, '_casestudy'):
+            self._casestudy = ""
+        else:
+            return self._casestudy
+
+   
+     
+                
+InitializeClass(CourseSubject)
+InitializeClass(CourseSubjectVersion)
---
+++ views/edit/VersionedContent/CourseSubject/kupusilvaconfiguration.js
@@ -0,0 +1,8 @@
+EDITABLE_METADATA = {
+    'http://infrae.com/namespaces/metadata/silva-news':
+    [
+      	{name: 'shortdescription', type: 'textarea', mandatory: 0, title: 'Short description'},
+        {name: 'image', type: 'reference', mandatory: 0, title: 'Image',lookup: 'Silva Image'},
+        {name: 'casestudy', type: 'reference', mandatory: 0, title: 'Case Study',lookup: 'Case Study'},
+    ]
+};
